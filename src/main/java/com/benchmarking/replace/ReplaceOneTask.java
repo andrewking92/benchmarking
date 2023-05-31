5d6536b505933898eabd5be748ac0f2c6c9b4806
andrewking92 <andrew.king@mongodb.com>
2023-05-31 11:21:24 +0100

    code cleanup

--- src/main/java/com/benchmarking/replace/ReplaceOneTask.java
+++ src/main/java/com/benchmarking/replace/ReplaceOneTask.java
@@ -5,7 +5,6 @@ import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
 import org.bson.conversions.Bson;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.result.UpdateResult;
 
 public class ReplaceOneTask implements Runnable {
     private final MongoCollection<Account> collection;
@@ -21,7 +20,7 @@ public class ReplaceOneTask implements Runnable {
         Account account = new Account("Dohn Joe", "abcdef", new SpecificAccountUsage("Specific Usage", "123 Main St", 10));
 
         try {
-            UpdateResult result = collection.replaceOne(filter, account);
+            collection.replaceOne(filter, account);
         } catch (MongoWriteException e) {
             System.err.println("Error during individual insert: " + e.getMessage());
         }
